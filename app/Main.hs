cd4ffde6d736d287205d66b265a23ae4eca21600
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-10-14 00:02:40 +0900

    Enable server

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main :: IO ()
-main = undefined
+import           Data.ByteString.Lazy     (ByteString)
+import           Network.HTTP.Types       (status202)
+import           Network.Wai              (Application, Request (..),
+                                           responseLBS)
+import           Network.Wai.Handler.Warp (run)
 
+
+main :: IO ()
+main = run 8000 server
+
+server :: Application
+server req respond =
+  case pathInfo req of
+    ["main.js"] ->
+      respond $ responseLBS
+      status202
+      [("Content-Type", "text/javascript")]
+      mainJs
+    _ ->
+      respond $ responseLBS
+      status202
+      [("Content-Type", "text/html")]
+      indexHtml
+
+mainJs :: ByteString
+mainJs = "main.js"
+
+indexHtml :: ByteString
+indexHtml = "index.html"
